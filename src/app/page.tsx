23bb27706294531aaf45b23c1ed6ec22d7941b67
Kechi Justice <kechi.regis@nokinc.com>
2026-06-24 11:43:43 +0100

    hero-section image rounded

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,6 +25,7 @@ export default function HomePage() {
           backgroundPosition: "center",
           backgroundRepeat: "no-repeat",
           backgroundBlendMode: "overlay",
+          borderRadius: "30px",
         }}
       >
         {/* Overlay */}
